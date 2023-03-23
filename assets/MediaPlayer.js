6c318314fa91ce4dc8b2ae5c9e145d9756fa0dc7
PepeCharly01 <jc.espinosa2@gmail.com>
2023-03-23 11:27:42 -0600

    promises, proxy and generators

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -5,16 +5,29 @@ function MediaPlayer(config) {
     this._initPlugins();
 }
 MediaPlayer.prototype._initPlugins = function () {
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        get mited() {
+            return this.media.muted;
+        },
+
+        set muted(value) {
+            this.media.muted = value;
+        },
+    };
+
     this.plugins.forEach(plugin => {
-        plugin.run(this);
+        plugin.run(player);
     });
-}
+};
 MediaPlayer.prototype.play = function() {
     this.media.play();
-}
+};
 MediaPlayer.prototype.pause = function() {
     this.media.pause();
-}
+};
 MediaPlayer.prototype.togglePlay = function() {
     this.media.paused 
     ? this.media.play()
--- assets/index.js
+++ assets/index.js
@@ -4,9 +4,8 @@ import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video");
 const player = new MediaPlayer({
     el: video, 
-    Plugins: [
-      //  new AutoPlay()
-    ]});
+    Plugins: [new AutoPlay()],
+  });
 
 const playButton = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
