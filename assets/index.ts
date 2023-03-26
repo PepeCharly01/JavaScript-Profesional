d2d13460d5ded92725556f45d329d51094df2bc9
PepeCharly01 <jc.espinosa2@gmail.com>
2023-03-26 15:33:44 -0600

    comvertir el proyecto a typescript

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
-import AutoPause from './plugins/AutoPause.js';
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({
@@ -8,10 +8,10 @@ const player = new MediaPlayer({
     Plugins: [new AutoPlay(), new AutoPause()],
   });
 
-const playButton = document.querySelector('#playButton');
+const playButton: HTMLElement = document.querySelector('#playButton') as HTMLElement;
 playButton.onclick = () => player.togglePlay();
 
-const muteButton = document.querySelector('#muteButton');
+const muteButton: HTMLElement = document.querySelector('#muteButton') as HTMLElement;
 muteButton.onclick = () => {
     player.media.muted
     ? player.unmute()
--- sw.js
+++ sw.js
@@ -21,14 +21,16 @@ self.addEventListener('fetch', event => {
 async function precache() {
   const cache = await caches.open(VERSION);
   return cache.addAll([
+    /*
     '/',
     '/index.html',
     '/assets/index.js',
     '/assets/MediaPlayer.js',
     '/assets/plugins/AutoPlay.js',
-    '/assets/plugins/AutoPause.js',
+    '/assets/plugins/AutoPause.ts',
     '/assets/index.css',
     '/assets/Les-Luthiers-Añoralgias.mp4',
+    */
   ]);
 }
 
